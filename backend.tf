7d3b6d2b46b616a5f25d7875eb356809d4deac72
mirek-ppup <mirek.svoboda@pointpickup.com>
2024-02-20 10:02:30 +0100

    Initial commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
              backend "s3" {
              key = "service_name/terraform.tfstate"
-                region = "us-east-1"
+                region = "eu-central-1"
                 bucket = "my-tf-test-bucket"
                }
               }
