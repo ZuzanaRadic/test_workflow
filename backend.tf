3593704e3d82fbcc11c3e749f5e6ab090bdb671d
mirek-ppup <mirek.svoboda@pointpickup.com>
2024-02-21 09:33:25 +0100

    Update backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
              backend "s3" {
-             key = "service_name/terraform.tfstate"
+             key = "service_name/prod_terraform.tfstate"
                 region = "eu-central-1"
                 bucket = "zuzana-tf-test-bucket"
                }
               }
-              
