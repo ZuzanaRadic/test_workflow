d886384634c6102b792e5db7f13d77e11516115a
mirek-ppup <mirek.svoboda@pointpickup.com>
2024-03-14 13:17:48 +0100

    Use lock terraform state

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
 terraform {
              backend "s3" {
+             encrypt = true 
+             dynamodb_table = "grocerkey-terraform-state-lock"
              key = "service_name/terraform.tfstate"
-                region = "eu-central-1"
-                bucket = "zuzana-tf-test-bucket"
+                region = "us-east-1"
+                bucket = "grocerkey-terraform-state"
                }
               }
               
