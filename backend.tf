930c23be4d802c463f295b5f1ceefa3605e738b3
mirek-ppup <mirek.svoboda@pointpickup.com>
2024-03-27 10:40:56 +0100

    Updates

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
              backend "s3" {
              encrypt = true 
-             dynamodb_table = "test-terraform-state-lock"
              key = "service_name/terraform.tfstate"
                 region = "eu-central-1"
                 bucket = "test-terraform-state"
